7a4fe999ae914e7366c578c37c9bb31b170949a8
benja-m0ra <133026464+benja-m0ra@users.noreply.github.com>
2023-07-01 18:01:28 -0400

    initial commit

---
+++ modal.js
@@ -0,0 +1,34 @@
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+open.addEventListener('click',()=>{
+    modal_container.classList.add('show');
+   
+});
+
+close.addEventListener('click',()=>{
+    modal_container.classList.remove('show');
+});
+
+//leer mas
+
+let hideText_btn = document.getElementById('hideText_btn');
+let hideText = document.getElementById('hideText');
+
+hideText_btn.addEventListener('click', toggleText);
+
+function toggleText(){
+    hideText.classList.toggle('mostrar');
+
+    if(hideText.classList.contains('mostrar')){
+        hideText_btn.innerHTML = 'leer menos';
+    }
+    else{
+        hideText_btn.innerHTML = 'leer mas';
+    }
+}
+
+
+
+
---
+++ nuevo_sabor/dom.js
@@ -0,0 +1,22 @@
+//registros de elecciones de chocolates
+
+const toppings = document.getElementsByClassName('topping');
+
+for(const topping of toppings){
+
+    topping.addEventListener('click',(e)=>{
+        console.log(e.target.innerText);
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+
